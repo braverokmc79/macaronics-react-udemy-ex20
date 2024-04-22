f85d80d562b30708e28708afce32c3142872bc7c
braverokmc79 <braverokmc79@gmail.com>
2024-04-22 21:34:54 +0900

    327.Link 로 페이지들 간에 이동하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}  />
+        <Route path="/products" element={<ProductsPage />}  />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
